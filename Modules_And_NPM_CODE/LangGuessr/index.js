89da9738ad2b050b2f792ba6eb3372e5a48dfc88
Natanael Pascal <npascal70@gmail.com>
2026-03-11 11:29:25 +0700

    feat:added express

---
+++ Modules_And_NPM_CODE/LangGuessr/index.js
@@ -0,0 +1,14 @@
+const franc = require('franc')
+const langs = require('langs')
+const colors = require('colors')
+const input = process.argv[2]
+const langCode = franc(input)
+
+if (langCode === 'und') {
+    console.log("Can't figure it out".rainbow)
+} else {
+    const language = langs.where('3', langCode)
+    console.log(`Our best guess is ${language.name.rainbow}`)
+
+}
+
---
+++ Modules_And_NPM_CODE/shelter/index.js
@@ -0,0 +1,6 @@
+const blue = require('./blue')
+const sadie = require('./sadie')
+const janet = require('./janet')
+
+const allCat = [blue, sadie, janet]
+module.exports = allCat
