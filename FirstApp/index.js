bcd78a9f0eab7a91fd3e6b3b80a37c9bb3781b88
Natanael Pascal <npascal70@gmail.com>
2026-03-11 16:29:15 +0700

    finished trying express

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -1,61 +1,64 @@
-// This download does not include the node_modules folder
-// REMEMBER TO RUN "npm install" first, to tell NPM to download the needed packages
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
+// all incoming request coming here
+// req = request (text information)
+// res = response
 // app.use((req, res) => {
-//     console.log("WE GOT A NEW REQUEST!!")
-//     res.send('<h1>This is my webpage!</h1>')
+//     console.log("we got new req")
+//     // res.send('<h1>This is my webpage!</h1>')
 // })
 
-
+// root route
 app.get('/', (req, res) => {
-    res.send('Welcome to the home page!')
+    res.send('This is the homepage!!!')
 })
 
+// use ':' to destinate the route
 app.get('/r/:subreddit', (req, res) => {
-    const { subreddit } = req.params;
-    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+    const { subreddit } = req.params // params mean parameter
+    res.send(`<h1> Browsing the ${subreddit} subreddit`)
 })
 
+// use ':' to destinate the route
 app.get('/r/:subreddit/:postId', (req, res) => {
-    const { subreddit, postId } = req.params;
-    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit</h1>`)
+    const { subreddit, postId } = req.params // params mean parameter
+    res.send(`<h1> Viewing PostID : ${postId} on ${subreddit} subreddit`)
 })
 
 app.post('/cats', (req, res) => {
-    res.send('POST REQUEST TO /cats!!!! THIS IS DIFFERENT THAN A GET REQUEST!')
+    res.send("THIS IS POST")
 })
 
 app.get('/cats', (req, res) => {
-    res.send('MEOW!!')
+    res.send('Meow')
 })
 
 app.get('/dogs', (req, res) => {
-    res.send('WOOF!')
+    res.send('Woof')
 })
+
 app.get('/search', (req, res) => {
-    const { q } = req.query;
+    const { q } = req.query
     if (!q) {
-        res.send('NOTHING FOUND IF NOTHING SEARCHED!')
-    } else {
-        res.send(`<h1>Search results for: ${q}</h1>`)
+        res.send("NOTHING FOUND")
     }
+    res.send(`<h1> Search result for ${q} </h1>`)
 })
 
-app.get('*', (req, res) => {
-    res.send(`I don't know that path!`)
+// always put this route on the bottom
+app.get('/*', (req, res) => {
+    res.send(`I don't know that path`)
 })
 
+// /r/SOMETHINGHERE
 
 
 
 // /cats => 'meow'
 // /dogs => 'woof'
-// '/' 
+// '/' => homepage
 
-
-
-app.listen(8080, () => {
-    console.log("LISTENING ON PORT 8080")
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
 })
