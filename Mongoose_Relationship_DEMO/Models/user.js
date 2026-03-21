79e00d71431b7d216727d525a38e1c3c3cbb72aa
Natanael Pascal <npascal70@gmail.com>
2026-03-22 00:12:55 +0700

    feat:finished on mongoose relationship

---
+++ Mongoose_Relationship_DEMO/Models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/relationshipDemo')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses : [
+        {
+            _id: {_id: false},
+            street: String,
+            city: String,
+            state: String,
+            country: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+})
+
+const User = mongoose.model('User', userSchema)
+
+const makeUser = async () =>{
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter'
+    })
+    u.addresses.push({
+        street: '123 Sesame Street',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    })
+    const res = await u.save()
+    console.log(res)
+}
+
+const addAddress = async(id) =>{
+    const user = await User.findById(id)
+    user.addresses.push({
+        street: '99 3rd Street',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    })
+    const res = await user.save()
+    console.log(res)
+}
+
+addAddress('69be60aab996d55a1e1dea7c')
